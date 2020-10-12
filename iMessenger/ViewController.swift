7695ae843d8efaf38627e4d90942b0b1dd0e1bdc
Libiki123 <atruong17@myseneca.ca>
2020-10-12 00:46:19 -0400

    getting started

---
+++ iMessenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iMessenger
+//
+//  Created by Nikki Truong on 2020-10-12.
+//  Copyright © 2020 EthanTruong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
