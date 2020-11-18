5a2c16e0ec187e1bcfe17afac4532b56f4cfe7bf
Libiki123 <atruong17@myseneca.ca>
2020-11-18 01:09:20 -0500

    setting up database for conversation

--- iMessenger/Controllers/ConversationsViewController.swift
+++ iMessenger/Controllers/ConversationsViewController.swift
@@ -35,12 +35,29 @@ class ConversationsViewController: UIViewController {
         view.addSubview(tableView)
         view.addSubview(noConversationsLabel)
         setupTableView()
+        fetchConversation()
     }
     
     @objc private func didTapComposeButton(){
         let vc = NewConverationViewController()
+        vc.completion = { [weak self] result in
+            self?.createNewConversation(result: result)
+        }
         let navVC = UINavigationController(rootViewController: vc)
         present(navVC, animated: true)
+    }
+    
+    private func createNewConversation(result: [String: String]) {
+        guard let name = result["name"],
+              let email = result["email"] else {
+            return
+        }
+        
+        let vc = ChatViewController(with: email)
+        vc.isNewConversation = true
+        vc.title = name
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -93,7 +110,7 @@ extension ConversationsViewController: UITableViewDelegate, UITableViewDataSourc
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        let vc = ChatViewController()
+        let vc = ChatViewController(with: "saddasdasd")
         vc.title = "Ethan"
         vc.navigationItem.largeTitleDisplayMode = .never
         navigationController?.pushViewController(vc, animated: true)
--- iMessenger/Controllers/NewConverationViewController.swift
+++ iMessenger/Controllers/NewConverationViewController.swift
@@ -11,6 +11,8 @@ import JGProgressHUD
 
 class NewConverationViewController: UIViewController {
     
+    public var completion: (([String: String]) -> (Void))?
+    
     private let spinner = JGProgressHUD(style: .dark)
     
     private var users = [[String: String]]()
@@ -88,6 +90,13 @@ extension NewConverationViewController:UITableViewDelegate, UITableViewDataSourc
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        // start conversation
+        let targetUserData = results[indexPath.row]
+        
+        dismiss(animated: true, completion: { [weak self] in
+            self?.completion?(targetUserData)
+        })
+        
         
     }
 }
